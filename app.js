2f53e1f57cf342289655e9b3171c281f3e4b065f
Mr-Isse <asadisse12@gmail.com>
2024-12-09 20:28:20 +0300

    Change Dark Mode and Ligth Mode

---
+++ app.js
@@ -0,0 +1,9 @@
+function SetDarkMode(){
+    document.body.style.backgroundColor="black"
+    document.body.style.color="white"
+}
+function SetLigthMode(){
+
+    document.body.style.backgroundColor="white"
+    document.body.style.color="black"
+}
